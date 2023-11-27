0771bfe3911136612f04ca0ab72036fc7c5d926f
danielfang001 <34532118+danielfang001@users.noreply.github.com>
2023-11-26 16:56:05 -0800

    Add files via upload

---
+++ constant.py
@@ -0,0 +1,25 @@
+#training dataset path
+TRAIN_FOLDER = r'C:\Users\danie\Desktop\PyProjects\UnsupervisedDeepImageStitching\VFIS-Net\training'
+
+#testing dataset path
+TEST_FOLDER = r'C:\Users\danie\Desktop\PyProjects\UnsupervisedDeepImageStitching\VFIS-Net\testing'
+
+#GPU index
+GPU = '1'
+
+#batch size for training
+TRAIN_BATCH_SIZE = 4
+
+#batch size for testing
+TEST_BATCH_SIZE = 1
+
+#num of iters (originally 600000)
+ITERATIONS = 2000
+
+# checkpoints path
+SNAPSHOT_DIR = "./checkpoints_homo"
+
+#sumary path
+SUMMARY_DIR = "./summary"
+
+
